27ba6281ae6548359703cf958f1486d69ec5fd9c
3x-haust <lyusungyun@icloud.com>
2024-09-06 14:12:15 +0900

    fix: 외부 접속 차단

--- backend/blog/src/main.ts
+++ backend/blog/src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestMiddleware, Injectable } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
+
+@Injectable()
+class IpRestrictionMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const allowedIps = ['127.0.0.1'];
+    const requesterIp = req.ip;
+
+    if (allowedIps.includes(requesterIp)) {
+      next();
+    }
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:80',
-  });
+  app.use(
+    (
+      req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+      res: Response<any, Record<string, any>>,
+      next: NextFunction,
+    ) => new IpRestrictionMiddleware().use(req, res, next),
+  );
   await app.listen(8000);
 }
 bootstrap();
