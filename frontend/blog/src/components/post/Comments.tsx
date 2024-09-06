023cba5a2e24dbca9583f3f4e56980e96ae69d6f
3x-haust <lyusungyun@icloud.com>
2024-09-06 11:05:12 +0900

    feat: 댓글 컴포넌트

---
+++ frontend/blog/src/components/post/Comments.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+const PostCommentsBlock = styled.div`
+  border: 1px solid var(--color-text);
+`
+
+export default function Comments() {
+  return (
+    <PostCommentsBlock>
+      asdf
+    </PostCommentsBlock>
+  )
+}
