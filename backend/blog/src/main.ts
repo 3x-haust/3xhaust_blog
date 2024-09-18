6ec5053566a3bb4ded3ccb4f129ca3c37868f0c2
3x-haust <lyusungyun@icloud.com>
2024-09-19 01:19:28 +0900

    fix: port

--- backend/blog/src/main.ts
+++ backend/blog/src/main.ts
@@ -7,6 +7,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(8080);
 }
 bootstrap();
