a96b1c1570853b105a19134ec600e5daa4fff5ad
3x-haust <lyusungyun@icloud.com>
2024-10-09 02:48:01 +0900

    fix: Prop  did not match bug

--- frontend/blog/next.config.mjs
+++ frontend/blog/next.config.mjs
@@ -2,6 +2,9 @@ const NEXT_PUBLIC_KEY = process.env.NEXT_PUBLIC_KEY;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
