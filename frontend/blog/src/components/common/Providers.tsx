077d9ad488d3ccd1cc32cbf6193bb8c0e45973c5
3x-haust <lyusungyun@icloud.com>
2024-10-09 02:47:24 +0900

    fix: Extra attributes from the server: data-theme,style bug

--- frontend/blog/src/components/common/Providers.tsx
+++ frontend/blog/src/components/common/Providers.tsx
@@ -3,8 +3,19 @@
 import { ThemeProvider } from 'next-themes';
 import { GlobalStyle } from '@/lib/styles/GlobalStyle';
 import Header from './Header';
+import { useEffect, useState } from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [isMount, setMount] = useState(false)
+
+  useEffect(() => {
+    setMount(true)
+  }, [])
+
+  if (!isMount) {
+    return null
+  }
+  
   return (
     <>
       <GlobalStyle />
