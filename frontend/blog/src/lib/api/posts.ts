3c57590dc75bb59dd096f4285669a17773a1721e
3x-haust <lyusungyun@icloud.com>
2024-10-11 03:13:03 +0900

    feat: implement new API routes for fetching posts and post details

--- frontend/blog/src/lib/api/posts.ts
+++ frontend/blog/src/lib/api/posts.ts
@@ -1,15 +1,11 @@
 import axios from 'axios';
 import { Post } from '@/types/post';
 
-const API_URL = 'https://api.3xhaust.dev/api';
+const API_URL = 'api';
 
 export async function getPosts(): Promise<Post[]> {
   try {
-    const response = await axios.get(`${API_URL}/posts`, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_KEY}`
-      }
-    });
+    const response = await axios.get(`${API_URL}/posts`);
     return response.data.data;
   } catch (error) {
     console.error('Error fetching posts:', error);
@@ -19,11 +15,7 @@ export async function getPosts(): Promise<Post[]> {
 
 export async function getPostByTitle(title: string): Promise<Post | null> {
   try {
-    const response = await axios.get(`${API_URL}/posts/title/${title.replace(/-/g, ' ')}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_KEY}`
-      }
-    });
+    const response = await axios.get(`${API_URL}/posts/title/${title.replace(/-/g, ' ')}`);
     return response.data.data;
   } catch (error) {
     console.error('Error fetching post:', error);
